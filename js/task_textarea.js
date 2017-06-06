a22751f18237cc81ba7780a46e4a995ada18edb7
volpe28v <naoki.koda@gmail.com>
2017-06-06 21:28:26 +0900

    fix #2. スペースとタブの区切りを選択可能にした

--- js/task_textarea.js
+++ js/task_textarea.js
@@ -1,21 +1,34 @@
 var taskTextarea = Vue.component('task-textarea',{
-  template: '<textarea v-model="text" placeholder="Title 30 Todo/Doing/Done Assignee"></textarea>',
+  template: '<div>\
+    <div class="sepa-mode">\
+      <input type="radio" id="space" value="space" v-model="sepaMode">\
+      <label for="space">Space</label>\
+      <input type="radio" id="tab" value="tab" v-model="sepaMode">\
+      <label for="tab">Tab</label>\
+    </div>\
+    <textarea v-model="text" placeholder="Title 30 Todo/Doing/Done Assignee"></textarea>\
+  </div>',
 
   props: [],
 
   data: function(){
     return {
+      sepaMode: "space",
       text: ""
     }
   },
 
   watch: {
+    sepaMode: function(){
+      this.updateText();
+    },
     text: function(){
       this.updateText();
     },
   },
 
   mounted: function(){
+    // デフォルトテキスト
     this.text = [
       "タスク1 30 Todo",
       "タスク2 40 Todo",
@@ -28,19 +41,28 @@ var taskTextarea = Vue.component('task-textarea',{
       "タスク9 35 Done",
       "タスク10 90 Done Bさん",
     ].join("\n");
-
   },
 
   methods: {
+    getReg: function(){
+      var self = this;
+
+      if (self.sepaMode == "space"){
+        return /(\S+)[ ]+([\d\.]+)([ ]+(\S+))?([ ]+(\S+))?/;
+      }else{
+        return /(.+)[\t]+([\d\.]+)([\t]+(\w+))?([\t]+(.+))?/;
+      }
+    },
+
     updateText: function(){
       var self = this;
+      var rowReg = self.getReg();
 
       var tasks = {
         "name": "root_dir",
         "children": null
       }
 
-      var rowReg = /(\S+)[ \t]+([\d\.]+)([ \t]+(\S+))?([ \t]+(\S+))?/;
       var children = self.text.split("\n")
         .filter(function(row){
           var matched = row.match(rowReg);
